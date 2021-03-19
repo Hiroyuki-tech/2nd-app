2a42d843d7da9d2e0b925ce1fcafa51652317868
Hiroyuki-tech <purapi71202000@yahoo.co.jp>
2021-03-19 22:55:16 +0900

    blade file　作成
    入力画面作成のため

--- resources/views/memo/list.blade.php
+++ resources/views/memo/list.blade.php
@@ -1,31 +1,6 @@
-<!DOCTYPE HTML>
-<html lang="ja">
-
-<head>
-  <meta charset="UTF-8">
-  <title>メモ</title>
-  <link rel="stylesheet" href="{{asset('css/app.css')}}">
-  <script src="/js/app.js" defer></script>
-</head>
-
-<body>
-  <header>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">メモ</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <a class="nav-item nav-link active" href="#">メモ一覧 <span class="sr-only"></span></a>
-          <a class="nav-item nav-link" href="#">メモ投稿</a>
-        </div>
-      </div>
-    </nav>
-  </header>
-  <br>
-  <div class="container">
-    <div class="row">
+@extends('layout')
+@section('content')
+<div class="row">
       <div class="col-md-8">
         <h2>メモ一覧</h2>
         <table class="table table-striped">
@@ -44,12 +19,4 @@
         </table>
       </div>
     </div>
-  </div>
-  <footer class="footer bg-dark  fixed-bottom">
-    <div class="container text-center">
-      <span class="text-light">memo</span>
-    </div>
-  </footer>
-</body>
-
-</html>
+@endsection
