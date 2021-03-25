a7c2ffdd257fe1865821a1e68b0f2a487a5601f8
Hiroyuki-tech <purapi71202000@yahoo.co.jp>
2021-03-25 21:23:15 +0900

    routing作成
    routing

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,15 @@ use App\Http\Controllers\MemoController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//一覧画面を表示
 Route::get('/',[MemoController::class,'showList'])->name('memos');
+
+//登録画面を表示
+Route::get('/memo/create',[MemoController::class,'showCreate'])->name('create');
+
+//メモ登録
+Route::post('/memo/store',[MemoController::class,'exeStore'])->name('store');
+
+
+//詳細画面を表示
 Route::get('/memo/{id}',[MemoController::class,'showDetail'])->name('show');
